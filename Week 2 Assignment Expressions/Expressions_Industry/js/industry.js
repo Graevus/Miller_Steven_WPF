168f6e337a1d3d7f3fad55afc6c1c59ee4dc1c48
Graevus <graevus427@gmail.com>
2014-02-12 14:55:21 -0600

    Second commit, getting the basics of the code down.

---
+++ Week 2 Assignment Expressions/Expressions_Industry/js/industry.js
@@ -0,0 +1,10 @@
+//Variables will be listed here.
+
+var length = prompt("Please input the length in feet here. Remember to round down.");
+var width = prompt("please input the width in feet here. Remember to round down.");
+var steps = prompt("please input your walking stride in feet here. Remember to round down.");
+
+//Calculation for the perimeter will be divided by the stride length here.
+var result = 2 * (length + width);
+
+console.log(result);
