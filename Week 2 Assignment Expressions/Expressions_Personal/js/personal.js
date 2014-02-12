d5f147001399e5b1675ad450c5baa5dff818c170
Graevus <graevus427@gmail.com>
2014-02-12 17:42:15 -0600

    Commit Fifteen. Found some capitalization errors.

--- Week 2 Assignment Expressions/Expressions_Personal/js/personal.js
+++ Week 2 Assignment Expressions/Expressions_Personal/js/personal.js
@@ -2,7 +2,7 @@
 
 //Variable will be declared here
 
-var bore=prompt("Please input Cylinder bore size here.\nCylinder bore is the diameter (in inches) of each cylinder");//Prompting the user to input bore size.
+var bore=prompt("Please input cylinder bore size here.\nCylinder bore is the diameter (in inches) of each cylinder");//Prompting the user to input bore size.
 var stroke=prompt("Please input the stroke (in inches) of the crankshaft.\nThe stroke is the distance each piston in the engine travels from bottom to top dead center.");//Prompting user to give the stroke length.
 var cyl=prompt("Please input the number of cylinders the engine has.");//Prompting user to input the number of cylinders in the engine.
 var pie= 0.785; //Declaring variable for pie divided by 4 to make my life easier.
