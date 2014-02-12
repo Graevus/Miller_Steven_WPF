c48c8122039727406d91215847277fb8392717f7
Graevus <graevus427@gmail.com>
2014-02-12 17:49:14 -0600

    Commit Seventeen. Continuing to clean up the display dialog.

--- Week 2 Assignment Expressions/Expressions_Personal/js/personal.js
+++ Week 2 Assignment Expressions/Expressions_Personal/js/personal.js
@@ -2,9 +2,9 @@
 
 //Variable will be declared here
 
-var bore=prompt("Please indicate cylinder bore size here.\nCylinder bore is the diameter (in inches) of each cylinder");//Prompting the user to input bore size.
+var bore=prompt("Please indicate the cylinder bore size.\nCylinder bore is the diameter (in inches) of each cylinder");//Prompting the user to input bore size.
 var stroke=prompt("Please define the stroke (in inches) of the crankshaft.\nThe stroke is the distance each piston in the engine travels from bottom to top dead center.");//Prompting user to give the stroke length.
-var cyl=prompt("Please specify the number of cylinders.");//Prompting user to input the number of cylinders in the engine.
+var cyl=prompt("Please determine the number of cylinders.");//Prompting user to input the number of cylinders in the engine.
 var pie= 0.785; //Declaring variable for pie divided by 4 to make my life easier.
 var bore2= bore*bore;//Declaring variable to square bore size.
 var result=[(pie*bore2)*stroke]*cyl//Declaring variable to calculate the displacement with given inputs.
