779a63a52e403ecd2afb39487ed019c106187ec3
Graevus <graevus427@gmail.com>
2014-02-12 16:12:04 -0600

    Commit # Thirteen. More tidying up, not much else I can change.

--- Week 2 Assignment Expressions/Expressions_Industry/js/industry.js
+++ Week 2 Assignment Expressions/Expressions_Industry/js/industry.js
@@ -1,16 +1,16 @@
 //Variables will be listed here.
 
-var length = prompt("Please input the length in feet. \nRemember to round down."); //Prompting for length of area.
-var width = prompt("Please input the width in feet. \nRemember to round down."); //Prompting for width of area.
-var steps = prompt("Please input your walking stride in feet. \nRemember to round down."); //Prompting for stride length.
+var length = prompt("Please input the length in feet. \nRemember to round down.");//Prompting for length of area.
+var width = prompt("Please input the width in feet. \nRemember to round down.");//Prompting for width of area.
+var steps = prompt("Please input your walking stride in feet. \nRemember to round down.");//Prompting for stride length.
 
 
-var result = 2 * (Number(length) + Number(width)) / Number(steps); //Calculation for the perimeter will be divided by the stride length here.
+var result = 2 * (Number(length) + Number(width)) / Number(steps);//Calculation for the perimeter will be divided by the stride length here.
 
 
 alert ("The estimated number of steps is "+result)//Declaring the result of the above line and giving it to the user in alert format.
 
 
-console.log("The estimated number of steps is "+result); //Prints the result variable to the console.
+console.log("The estimated number of steps is "+result);//Prints the result variable to the console.
 
 //My apologies if I'm a little heavy on the parentheses, but it was the only way I could get this to work.
