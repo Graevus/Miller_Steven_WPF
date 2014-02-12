a4c96af47985ef6a60621ca71614bd252527eadc
Graevus <graevus427@gmail.com>
2014-02-12 16:29:24 -0600

    Second Commit. Declaring my first variables

--- Week 2 Assignment Expressions/Expressions_Personal/js/personal.js
+++ Week 2 Assignment Expressions/Expressions_Personal/js/personal.js
@@ -1,1 +1,5 @@
-alert("This is in the js file");
+//This code should give the use the displacement in cubic inches of an engine given the information provided.
+
+//Variable will be declared here
+
+var bore=prompt("Insert Cylinder bore size here.\nCylinder bore is the diameter (in inches) of each cylinder")
