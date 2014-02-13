65958c3c1314c52402ad711b993846cfbfcf3d5c
Graevus <graevus427@gmail.com>
2014-02-12 20:06:12 -0600

    Commit Six. Declaring variables.

--- Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
+++ Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
@@ -1,1 +1,10 @@
-//
+//Displaying the number of padded sheets and size of sheet to cover surface area of a given room
+
+//Declaring my variables
+
+var length=prompt("Please input the length of the room in feet.");
+var width=prompt("Please input the width of the room in feet.");
+var height=prompt("please input the height of the room in feet.");
+var sheet=[]
+
+
