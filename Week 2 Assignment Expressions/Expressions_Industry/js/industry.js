53a454c607f86f6c9cc552edb51aded73171ac10
Graevus <graevus427@gmail.com>
2014-02-12 18:27:35 -0600

    Final Commit. Finished wording changes.

--- Week 2 Assignment Expressions/Expressions_Industry/js/industry.js
+++ Week 2 Assignment Expressions/Expressions_Industry/js/industry.js
@@ -5,7 +5,7 @@
 
 var length = prompt("Please indicate the length in feet. \nRemember to round down.");//Prompting for length of area.
 var width = prompt("Please determine the width in feet. \nRemember to round down.");//Prompting for width of area.
-var steps = prompt("Please input your walking stride in feet. \nRemember to round down.");//Prompting for stride length.
+var steps = prompt("Please estimate your walking stride in feet. \nRemember to round down.");//Prompting for stride length.
 
 
 var result = 2 * (Number(length) + Number(width)) / Number(steps);//Calculation for the perimeter will be divided by the stride length here.
