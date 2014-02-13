75255727b29bcba6a86dd69bb7c9c4ea9f6ed74a
Graevus <graevus427@gmail.com>
2014-02-12 18:41:17 -0600

    Commit Two. Declaring my variables.

--- Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
+++ Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
@@ -1,2 +1,8 @@
 //This script should calculate the number of bottle of paint used in a year by a miniature painter.
 
+//Variable will be declared here
+
+var mini=prompt()
+var paint=prompt("please input bottle size")
+var mini=prompt()
+
