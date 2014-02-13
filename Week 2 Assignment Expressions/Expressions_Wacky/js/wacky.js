6b763e0a09d6526eb61a2da205acbd3bf44d79cd
Graevus <graevus427@gmail.com>
2014-02-12 20:35:30 -0600

    Commit Eight. Working out more of the code. Still need to add notes to my code lines.

--- Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
+++ Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
@@ -5,8 +5,9 @@
 var length=prompt("Please input the length of the room in feet.");
 var width=prompt("Please input the width of the room in feet.");
 var height=prompt("please input the height of the room in feet.");
-var sheet=[]
+var sheet=[32,36,40,48]
 var room=2*length*width+2*width*height+2*length*height
+var result
 
 console. log(room)
 
