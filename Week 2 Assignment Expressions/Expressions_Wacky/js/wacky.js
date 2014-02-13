c3a130d8088e8f946d6ab678c528cf4c69ca4750
Graevus <graevus427@gmail.com>
2014-02-13 17:25:47 -0600

    Commint Ten. Worked out my bugs, now just finishing up some display work.

--- Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
+++ Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
@@ -5,10 +5,10 @@
 var length=prompt("Please input the length of the room in feet.");
 var width=prompt("Please input the width of the room in feet.");
 var height=prompt("please input the height of the room in feet.");
-var sheet=[32,36,40,48]
+var sheet=[ 32,36,40,48 ]
 var room=2*length*width+2*width*height+2*length*height
-var result1=Number(room)/[0]
+var result1=room/sheet[0]
 
-console. log(result1)
+console. log("You will need "+Math.round(result1)+" 4x8 sheets.\nYou will need "+Math.round(result1)+" 4x9 sheets.\nYou will need "+Math.round(result1)+" 4x10 sheets.\nYou")
 
 
