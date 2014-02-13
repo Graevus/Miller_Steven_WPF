4e243d55a159566c44551a9ba930c7a23685310c
Graevus <graevus427@gmail.com>
2014-02-13 17:42:09 -0600

    Commit Eleven. Adding in my notes and cleaning up a bit.

--- Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
+++ Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
@@ -2,13 +2,17 @@
 
 //Declaring my variables
 
-var length=prompt("Please input the length of the room in feet.");
-var width=prompt("Please input the width of the room in feet.");
-var height=prompt("please input the height of the room in feet.");
-var sheet=[ 32,36,40,48 ]
-var room=2*length*width+2*width*height+2*length*height
-var result1=room/sheet[0]
+var length=prompt("Please input the length of the room in feet.");//Prompting user for length of room in feet.
+var width=prompt("Please input the width of the room in feet.");//Prompting user for width of room in feet.
+var height=prompt("please input the height of the room in feet.");//Prompting user for height of room in feet.
+var sheet=[ 32,36,40,48 ];//Setting up an array for the surface area of sheet sizes.
+var room=2*length*width+2*width*height+2*length*height;//calculating the surface area of the room using the measurements given by the user.
+var result1=room/sheet[0];//Calculating the number of sheets needed using 4'x8' sheets.
+var result2=room/sheet[1];
+var result3=room/sheet[2];
+var result4=room/sheet[3];
 
-console. log("You will need "+Math.round(result1)+" 4x8 sheets.\nYou will need "+Math.round(result1)+" 4x9 sheets.\nYou will need "+Math.round(result1)+" 4x10 sheets.\nYou")
+//Dis[lay Functions
+console. log("You will need "+Math.round(result1)+" 4'x8' sheets.\nYou will need "+Math.round(result2)+" 4'x9' sheets.\nYou will need "+Math.round(result3)+" 4'x10' sheets.\nYou will need "+Math.round(result4)+" 4'x12' sheets.");
 
 
