7715d84b6c092e5ad3190ef39e4ba8ac8ad38b1e
Graevus <graevus427@gmail.com>
2014-02-13 17:55:22 -0600

    Commit Seventeen. Changing some wording in the display to the final product.

--- Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
+++ Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
@@ -2,7 +2,7 @@
 
 //Declaring my variables
 
-var length=prompt("Please input the length of the room in feet.");//Prompting user for length of room in feet.
+var length=prompt("Please indicate the length of the room in feet.");//Prompting user for length of room in feet.
 var width=prompt("Please input the width of the room in feet.");//Prompting user for width of room in feet.
 var height=prompt("please input the height of the room in feet.");//Prompting user for height of room in feet.
 var sheet=[ 32,36,40,48 ];//Setting up an array for the surface area of sheet sizes.
@@ -16,8 +16,8 @@ var result4=room/sheet[3];//Calculating the number of sheets needed using 4'x12'
 
 //Display Functions
 
-//alert("You will need "+Math.round(result1)+" 4'x8' sheets.\nYou will need "+Math.round(result2)+" 4'x9' sheets.\nYou will need "+Math.round(result3)+" 4'x10' sheets.\nYou will need "+Math.round(result4)+" 4'x12' sheets.");//My alret display function, dunno why I just like adding it.
-
 console. log("You will need "+Math.round(result1)+" 4'x8' sheets.\nYou will need "+Math.round(result2)+" 4'x9' sheets.\nYou will need "+Math.round(result3)+" 4'x10' sheets.\nYou will need "+Math.round(result4)+" 4'x12' sheets.");//Displaying the answer on the console for the user.
+
+//alert("You will need "+Math.round(result1)+" 4'x8' sheets.\nYou will need "+Math.round(result2)+" 4'x9' sheets.\nYou will need "+Math.round(result3)+" 4'x10' sheets.\nYou will need "+Math.round(result4)+" 4'x12' sheets.");//My alret display function, dunno why I just like adding it.
 
 
