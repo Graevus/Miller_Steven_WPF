b4152221e24fb81ffb495a97904837a25ea6037b
Graevus <graevus427@gmail.com>
2014-02-12 19:54:28 -0600

    Commit Five. Scrubbing that idea. Found next one.

--- Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
+++ Week 2 Assignment Expressions/Expressions_Wacky/js/wacky.js
@@ -1,10 +1,1 @@
-//We will calculate the estimated number of cans of paint needed to paint a room. Given each can is about 3 gallons
-
-
-//Declaring variables
-
-var length=prompt("Please enter the length of the room in inches.");
-var width=prompt("Please enter the width of the room in inches");
-var paint=prompt("Please enter the size of the paint can in gallons.");
-
-//Calculating the sq/ft of the room.
+//
