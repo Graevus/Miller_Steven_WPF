38dccc7eda14d4a26692a27fe750a9c1d69feec6
Graevus <graevus427@gmail.com>
2014-02-12 18:17:57 -0600

    Commit Sixteen. Adding notes.

--- Week 2 Assignment Expressions/Expressions_Industry/js/industry.js
+++ Week 2 Assignment Expressions/Expressions_Industry/js/industry.js
@@ -1,5 +1,6 @@
 
 //This should tell the user how many steps it would take to walk a perimeter.
+
 //Variables will be listed here.
 
 var length = prompt("Please input the length in feet. \nRemember to round down.");//Prompting for length of area.
@@ -9,6 +10,7 @@ var steps = prompt("Please input your walking stride in feet. \nRemember to roun
 
 var result = 2 * (Number(length) + Number(width)) / Number(steps);//Calculation for the perimeter will be divided by the stride length here.
 
+//Display functions.
 
 //alert ("The estimated number of steps is "+result)//Declaring the result of the above line and giving it to the user in alert format. disabling this in case you don't want it.
 
