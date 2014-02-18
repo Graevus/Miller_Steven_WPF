eb6c11f0cfdefaca3c63255f4a89ad53f12c82c5
Graevus <graevus427@gmail.com>
2014-02-18 17:08:23 -0600

    Commit six. setting up the else if, and adding notes.

--- Week 3 Assignment Conditionals/Conditionals_Personal/js/personal.js
+++ Week 3 Assignment Conditionals/Conditionals_Personal/js/personal.js
@@ -11,12 +11,13 @@ var cMileage = prompt("What is the current mileage of the vehicle?")
 
 var oil = 3000
 
-var oilC = (lMileage + oil);
+var oilC = (lMileage + oil);//Number of miles the average oil change lasts.
 
 
 
 //Display functions
 
 
-if (oilC <= cMileage)console.log("You need to get an oil change.")//If statement is true prints to console "You need to get an oil change."
-else console.log("You do not need an oil change.");//If the above statement is false then prints to console "You do not need an oil change."
+if (oilC < cMileage)console.log("You need to get an oil change.")//If statement is true prints to console "You need to get an oil change."
+    else console.log("You do not need an oil change.");//If the above statement is false then prints to console "You do not need an oil     change."
+if (oilC == cMileage)console.log("You could use an oil change, but you can put it off a few days too.")//If the statement is true then print to console "You could use an oil change, but you can put it off a few days too."
