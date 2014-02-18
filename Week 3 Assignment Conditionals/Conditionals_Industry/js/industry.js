4533e9ae88d96156fbfcb2d982826f1d3ded7836
Graevus <graevus427@gmail.com>
2014-02-18 16:12:50 -0600

    Commit one. Setting up initials code structure and testing calculations.

--- Week 3 Assignment Conditionals/Conditionals_Industry/js/industry.js
+++ Week 3 Assignment Conditionals/Conditionals_Industry/js/industry.js
@@ -1,15 +1,20 @@
 //Steven Miller 2/18/14
-//
+//prisoner release date
 
 //Variables will be listed here.
 
-var  = prompt("");//Prompting for .
-var  = prompt("");//Prompting for .
-var  = prompt("");//Prompting for .
+var sRelease = prompt("What is the scheduled release date of the prisoner?");//Prompting for .
+    if (sRelease == ("")){prompt ("Determine the scheduled release date.")}
+var  goodB= prompt("How many good behavior days has the prisoner accumulated?");//Prompting for .
+    if (goodB == ("")){prompt ("Determine the number of good behavior days here.")}
+//var date = prompt("What is today's date?")
+    if (date == ("")){prompt ("Please give the date here.")}
 
+//Calculating the release date of the prisoner
 
+var nRelease = sRelease - goodB
 
 //Display functions.
 
-console.log("");//Prints the result variable to the console.
+console.log(nRelease);//Prints the result variable to the console.
 
