35c37aed297b5569d43ff44686f52e389de9c0a7
Graevus <graevus427@gmail.com>
2014-02-18 16:49:26 -0600

    Commit one. Writing the variables.

---
+++ Week 3 Assignment Conditionals/Conditionals_Personal/js/personal.js
@@ -0,0 +1,12 @@
+//Steven Miller 2/18/14
+//Calculate if you need an oil change
+
+//Variable will be declared here
+
+var lMileage = prompt("What was the mileage of the vehicle at last oil change?");
+    if (lMileage == ("")){var lMileage = prompt ("Determine the mileage of last oil change here.");}
+var
+
+//Display functions
+
+console.log ("");//displaying the result in the console for the user to see.
