a6d9fa1f1b3a9f675ef56a5560c271422b643337
Graevus <graevus427@gmail.com>
2014-02-18 17:00:55 -0600

    Commit five. adding some notes.

--- Week 3 Assignment Conditionals/Conditionals_Personal/js/personal.js
+++ Week 3 Assignment Conditionals/Conditionals_Personal/js/personal.js
@@ -5,8 +5,10 @@
 
 var lMileage = prompt("What was the mileage of the vehicle at last oil change?")
     if (lMileage == ("")){var lMileage = prompt ("Determine the mileage of last oil change here.");}
+
 var cMileage = prompt("What is the current mileage of the vehicle?")
     if (cMileage == ("")){var cMileage = prompt ("Determine the mileage of the vehicle.");}
+
 var oil = 3000
 
 var oilC = (lMileage + oil);
@@ -16,5 +18,5 @@ var oilC = (lMileage + oil);
 //Display functions
 
 
-if (oilC <= cMileage)console.log("You need to get an oil change.")//If statement is true prints to console "you may release the prisoner."
-else console.log("You do not need an oil change.");//If the above statement is false then prints to console "You may not release the prisoner."
+if (oilC <= cMileage)console.log("You need to get an oil change.")//If statement is true prints to console "You need to get an oil change."
+else console.log("You do not need an oil change.");//If the above statement is false then prints to console "You do not need an oil change."
