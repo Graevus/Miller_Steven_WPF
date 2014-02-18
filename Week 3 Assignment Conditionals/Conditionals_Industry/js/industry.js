4f6962b4aa27bc4922fb880a4b591ee881b67af7
Graevus <graevus427@gmail.com>
2014-02-18 16:42:47 -0600

    Commit Five. Final notes, final commit

--- Week 3 Assignment Conditionals/Conditionals_Industry/js/industry.js
+++ Week 3 Assignment Conditionals/Conditionals_Industry/js/industry.js
@@ -6,14 +6,17 @@
 
 var sRelease = prompt("What day of this month is the prisoner scheduled to be released?");//Prompting for the scheduled release day .
     if (sRelease == ("")){var sRelease = prompt ("Determine the scheduled release day.");}//In case the previous prompt comes back empty the user is given another prompt to give the information in.
+
 var  goodB = prompt("How many good behavior days has the prisoner accumulated?");//Prompting for number of good behavior days the prisoner has accumulated.
     if (goodB == ("")){var goodB = prompt ("Determine the number of good behavior days here.");}//In case the previous prompt comes back empty the user is given another prompt to give the information in.
+
 var date = prompt("What is today's date?");//Prompting for the current day.
     if (date == ("")){var date = prompt ("Please give the date here.");}//In case the previous prompt comes back empty the user is given another prompt to give the information in.
 
 var nRelease = (sRelease - goodB);//Calculating the release date of the prisoner
 
 //Display functions.
-if (nRelease <= date)console.log("You may release the prisoner.")
+
+if (nRelease <= date)console.log("You may release the prisoner.")//If statement is true prints to console "you may release the prisoner."
 else console.log("You may not release the prisoner.");//If the above statement is false then prints to console "You may not release the prisoner."
 
