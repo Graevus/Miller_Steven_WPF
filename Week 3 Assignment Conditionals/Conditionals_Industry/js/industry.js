3f9a01282f2b418dab8e855466b50106af339b4f
Graevus <graevus427@gmail.com>
2014-02-18 15:31:37 -0600

    Initial upload for file structure.

---
+++ Week 3 Assignment Conditionals/Conditionals_Industry/js/industry.js
@@ -0,0 +1,15 @@
+//Steven Miller 2/18/14
+//
+
+//Variables will be listed here.
+
+var  = prompt("");//Prompting for .
+var  = prompt("");//Prompting for .
+var  = prompt("");//Prompting for .
+
+
+
+//Display functions.
+
+console.log("");//Prints the result variable to the console.
+
---
+++ Week 3 Assignment Conditionals/Conditionals_Wacky/js/wacky.js
@@ -0,0 +1,16 @@
+//Steven Miller 2/18/14
+//
+
+//Declaring my variables
+
+var
+var
+var
+var
+
+
+
+//Display Functions
+
+console. log("");//Displaying the answer on the console for the user.
+
