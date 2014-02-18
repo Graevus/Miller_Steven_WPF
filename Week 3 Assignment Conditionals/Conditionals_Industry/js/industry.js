659029f091692644869bc2039ff1956a2e5a5458
Graevus <graevus427@gmail.com>
2014-02-18 16:30:38 -0600

    Commit two. Finishing the basics of the code, seems to work. On to the finishing touches.

--- Week 3 Assignment Conditionals/Conditionals_Industry/js/industry.js
+++ Week 3 Assignment Conditionals/Conditionals_Industry/js/industry.js
@@ -1,20 +1,21 @@
 //Steven Miller 2/18/14
-//prisoner release date
+//prisoner release date given the scheduled release date is in the current month and year.
 
 //Variables will be listed here.
 
-var sRelease = prompt("What is the scheduled release date of the prisoner?");//Prompting for .
-    if (sRelease == ("")){prompt ("Determine the scheduled release date.")}
-var  goodB= prompt("How many good behavior days has the prisoner accumulated?");//Prompting for .
-    if (goodB == ("")){prompt ("Determine the number of good behavior days here.")}
-//var date = prompt("What is today's date?")
-    if (date == ("")){prompt ("Please give the date here.")}
+var sRelease = prompt("What day of this month is the prisoner scheduled to be released?");//Prompting for .
+    if (sRelease == ("")){var sRelease = prompt ("Determine the scheduled release day.")}
+var  goodB = prompt("How many good behavior days has the prisoner accumulated?");//Prompting for .
+    if (goodB == ("")){var goodB = prompt ("Determine the number of good behavior days here.")}
+var date = prompt("What is today's date?")
+    if (date == ("")){var date = prompt ("Please give the date here.")}
 
 //Calculating the release date of the prisoner
 
 var nRelease = sRelease - goodB
 
 //Display functions.
-
-console.log(nRelease);//Prints the result variable to the console.
+if (nRelease <= date)
+    console.log("You may release the prisoner.");//Prints the result variable to the console.
+else console.log("You may not release the prisoner.")
 
