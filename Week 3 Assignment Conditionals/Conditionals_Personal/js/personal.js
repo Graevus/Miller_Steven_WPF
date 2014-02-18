880a5348c886215653173c2f0bcc46e83ca5a561
Graevus <graevus427@gmail.com>
2014-02-18 17:25:09 -0600

    Commit nine. Fixing a few errors.

--- Week 3 Assignment Conditionals/Conditionals_Personal/js/personal.js
+++ Week 3 Assignment Conditionals/Conditionals_Personal/js/personal.js
@@ -9,15 +9,15 @@ var lMileage = prompt("What was the mileage of the vehicle at last oil change?")
 var cMileage = prompt("What is the current mileage of the vehicle?")
     if (cMileage == ("")){var cMileage = prompt ("Determine the mileage of the vehicle.");}
 
-var oil = 3000
+var oil = 3000//Number of miles the average oil change lasts.
 
-var oilC = (lMileage + oil);//Number of miles the average oil change lasts.
+var oilC = (lMileage + oil);
 
 
 
 //Display functions
 
 
-if (oilC == cMileage)console.log("You need to get an oil change, but your safe to put it off for a few days.")//If statement is true prints to console "You need to get an oil change."
-    else if (oilC < cMileage)console.log("You do not need an oil change.");//If the  statement is true then prints to console "You do not need an oil     change." but only if previous statement is false.
-else console.log("You could use an oil change, but you can put it off a few days too.")//If the previous two statements are false then print to console "You could use an oil change, but you can put it off a few days too."
+if (oilC == cMileage){console.log("You need to get an oil change, but your safe to put it off for a few days.")}//If statement is true prints to console "You need to get an oil change."
+    else if (oilC < cMileage){console.log("You do not need an oil change.");}//If the  statement is true then prints to console "You do not need an oil     change." but only if previous statement is false.
+else {console.log("You could use an oil change, but you can put it off a few days too.")}//If the previous two statements are false then print to console "You could use an oil change, but you can put it off a few days too."
