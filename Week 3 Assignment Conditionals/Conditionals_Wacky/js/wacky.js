a2263f93d6db35366d1280a89636ac2ceea31267
Graevus <graevus427@gmail.com>
2014-02-18 18:35:20 -0600

    Commit three. more notes.

--- Week 3 Assignment Conditionals/Conditionals_Wacky/js/wacky.js
+++ Week 3 Assignment Conditionals/Conditionals_Wacky/js/wacky.js
@@ -8,14 +8,17 @@ var movie = prompt("How much do you spend going to the movies?");//Asking for am
 
 var game = prompt ("how much do you spend on games.");//Asking user for amount spent on gaming.
     if (game == ("")){var game = prompt ("You forgot to tell me how much you spend on games.");}//Asking for amount spent at movies again if user left prompt blank first time.
+
 var wow = 200;//there for comparison to the result.
-var result = Number(movie)+Number(game);
 
-    (wow<=result) ? console.log("Your spending "+result+" dollars on entertainment. Wow! That's a lot!") : console.log("You spend about "+result+" dollars on entertainment.");
-
-
+var result = Number(movie)+Number(game);//calculating the amount spent on entertainment.
 
 //Display Functions
+    (wow<=result) ? console.log("Your spending "+result+" dollars on entertainment. Wow! That's a lot!") : console.log("You spend about "+result+" dollars on entertainment.");//if the result is greater than the "wow" variable then print message 1 if not then print message 2.
+
+
+
+
 
 
 
