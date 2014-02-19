ccd7920a06128cbee19803c0f84abc537b4d9a5f
Graevus <graevus427@gmail.com>
2014-02-18 18:06:12 -0600

    Commit one. Setting up my variables.

--- Week 3 Assignment Conditionals/Conditionals_Wacky/js/wacky.js
+++ Week 3 Assignment Conditionals/Conditionals_Wacky/js/wacky.js
@@ -1,10 +1,12 @@
 //Steven Miller 2/18/14
-//
+//Calculate the amount you spend on entertainment
 
 //Declaring my variables
 
-var
-var
+var movie = prompt("How much do you spend going to the movies?");//Asking for amount spent at movies.
+    if (movie == ("")){var lMileage = prompt ("You forgot to tell me how much you spend at the movies.");}//Asking for amount spent at movies again if user left prompt blank first time.
+
+var game = prompt ("how much do you spend on games.")//Asking user for amount spent on gaming.
 var
 var
 
