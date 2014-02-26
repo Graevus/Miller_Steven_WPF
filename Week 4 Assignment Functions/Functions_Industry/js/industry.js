7b6e3f22e86a6c6741af78fe835f1c09acbbe8c8
Graevus <graevus427@gmail.com>
2014-02-26 15:48:58 -0600

    Commit One. Setting up file structure and outlining code.

---
+++ Week 4 Assignment Functions/Functions_Industry/js/industry.js
@@ -0,0 +1,19 @@
+
+//Steven Miller
+//02/26/14
+
+var weekOne  = prompt("How many incident reports in week one? ");
+    if (weekOne == ("")){var weekOne = prompt ("How many incident reports in week one?");}
+
+var weekTwo  = prompt("How many incident reports in week one? ");
+    if (weekTwo == ("")){var weekTwo = prompt ("How many incident reports in week one?");}
+
+var weekThree  = prompt("How many incident reports in week one? ");
+    if (weekThree == ("")){var weekThree = prompt ("How many incident reports in week one?");}
+
+var weekFour  = prompt("How many incident reports in week one? ");
+    if (weekFour == ("")){var weekFour = prompt ("How many incident reports in week one?");}
+
+function result (weekOne, weekTwo, weekThree, weekFour){
+
+}
