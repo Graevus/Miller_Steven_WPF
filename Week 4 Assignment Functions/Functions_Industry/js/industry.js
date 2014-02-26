7633b02f3125fd68dd32238a340d84a8a2769d9b
Graevus <graevus427@gmail.com>
2014-02-26 16:16:11 -0600

    Commit Two. Figuring out functions.

--- Week 4 Assignment Functions/Functions_Industry/js/industry.js
+++ Week 4 Assignment Functions/Functions_Industry/js/industry.js
@@ -5,15 +5,23 @@
 var weekOne  = prompt("How many incident reports in week one? ");
     if (weekOne == ("")){var weekOne = prompt ("How many incident reports in week one?");}
 
-var weekTwo  = prompt("How many incident reports in week one? ");
-    if (weekTwo == ("")){var weekTwo = prompt ("How many incident reports in week one?");}
+var weekTwo  = prompt("How many incident reports in week two? ");
+    if (weekTwo == ("")){var weekTwo = prompt ("How many incident reports in week two?");}
 
-var weekThree  = prompt("How many incident reports in week one? ");
-    if (weekThree == ("")){var weekThree = prompt ("How many incident reports in week one?");}
+var weekThree  = prompt("How many incident reports in week three? ");
+    if (weekThree == ("")){var weekThree = prompt ("How many incident reports in week three?");}
 
-var weekFour  = prompt("How many incident reports in week one? ");
-    if (weekFour == ("")){var weekFour = prompt ("How many incident reports in week one?");}
+var weekFour  = prompt("How many incident reports in week four? ");
+    if (weekFour == ("")){var weekFour = prompt ("How many incident reports in week four?");}
 
-function result (weekOne, weekTwo, weekThree, weekFour){
+
+
+var total = result(weekOne,weekTwo,weekThree,weekFour);
+
+function result (a,b,c,d){
+    var add = Number(a)+Number(b)+Number(c)+Number(d);
+    return add;
 
 }
+
+console.log(total);
