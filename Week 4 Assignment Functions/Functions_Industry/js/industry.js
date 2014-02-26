4fdd1e36750389f8a59a7ac72dc04ec024cb3c16
Graevus <graevus427@gmail.com>
2014-02-26 17:24:36 -0600

    Commit Four. Adding comments.

--- Week 4 Assignment Functions/Functions_Industry/js/industry.js
+++ Week 4 Assignment Functions/Functions_Industry/js/industry.js
@@ -3,16 +3,16 @@
 //02/26/14
 //Functions - Industry
 
-var weekOne  = prompt("How many incident reports in week one? ");
+var weekOne  = prompt("How many incident reports in week one? ");//Prompting user for incidents in week one.
     if (weekOne == ("")){var weekOne = prompt ("How many incident reports in week one?");}
 
-var weekTwo  = prompt("How many incident reports in week two? ");
+var weekTwo  = prompt("How many incident reports in week two? ");//Prompting user for incidents in week two.
     if (weekTwo == ("")){var weekTwo = prompt ("How many incident reports in week two?");}
 
-var weekThree  = prompt("How many incident reports in week three? ");
+var weekThree  = prompt("How many incident reports in week three? ");//Prompting user for incidents in week three.
     if (weekThree == ("")){var weekThree = prompt ("How many incident reports in week three?");}
 
-var weekFour  = prompt("How many incident reports in week four? ");
+var weekFour  = prompt("How many incident reports in week four? ");//Prompting user for incidents in week four.
     if (weekFour == ("")){var weekFour = prompt ("How many incident reports in week four?");}
 
 
@@ -32,4 +32,4 @@ var calc = function(a,b){
 var average = calc(total,4);
 
 
-console.log("You have had "+total+"incidents this month. The is an average of "+average+" incidents per week.");
+console.log("You have had "+total+" incidents this month. The is an average of "+average+" incidents per week.");
