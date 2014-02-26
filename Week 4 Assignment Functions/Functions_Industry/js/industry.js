3dad11a609b6820574df8c13bef1955a542e092c
Graevus <graevus427@gmail.com>
2014-02-26 17:26:49 -0600

    Commit Five. Adding more comments

--- Week 4 Assignment Functions/Functions_Industry/js/industry.js
+++ Week 4 Assignment Functions/Functions_Industry/js/industry.js
@@ -4,16 +4,16 @@
 //Functions - Industry
 
 var weekOne  = prompt("How many incident reports in week one? ");//Prompting user for incidents in week one.
-    if (weekOne == ("")){var weekOne = prompt ("How many incident reports in week one?");}
+    if (weekOne == ("")){var weekOne = prompt ("How many incident reports in week one?");}//Repeating the prompt in case the prompt was left blank the first time.
 
 var weekTwo  = prompt("How many incident reports in week two? ");//Prompting user for incidents in week two.
-    if (weekTwo == ("")){var weekTwo = prompt ("How many incident reports in week two?");}
+    if (weekTwo == ("")){var weekTwo = prompt ("How many incident reports in week two?");}//Repeating the prompt in case the prompt was left blank the first time.
 
 var weekThree  = prompt("How many incident reports in week three? ");//Prompting user for incidents in week three.
-    if (weekThree == ("")){var weekThree = prompt ("How many incident reports in week three?");}
+    if (weekThree == ("")){var weekThree = prompt ("How many incident reports in week three?");}//Repeating the prompt in case the prompt was left blank the first time.
 
 var weekFour  = prompt("How many incident reports in week four? ");//Prompting user for incidents in week four.
-    if (weekFour == ("")){var weekFour = prompt ("How many incident reports in week four?");}
+    if (weekFour == ("")){var weekFour = prompt ("How many incident reports in week four?");}//Repeating the prompt in case the prompt was left blank the first time.
 
 
 
