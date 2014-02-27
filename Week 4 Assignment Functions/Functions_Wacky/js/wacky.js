3fe32bf96463322041f598e502c38ddfacc883db
Graevus <graevus427@gmail.com>
2014-02-26 18:53:28 -0600

    Commit two. Setting up basic code.

--- Week 4 Assignment Functions/Functions_Wacky/js/wacky.js
+++ Week 4 Assignment Functions/Functions_Wacky/js/wacky.js
@@ -1,3 +1,16 @@
 //Steven Miller
 //02/26/14
 //Functions - Wacky
+
+var ppl  = prompt("Are there other people around? Answer yes or no, no caps.");//Prompting user for the size of gas tank.
+    if (ppl == ("yes")){var ppl = 1}
+    else if (ppl == ("no")){var ppl = 0}
+    else (ppl == ("")){var ppl = prompt ("Are there other people around? Answer yes or no, no caps.");}
+
+var funny = prompt("Would it be funny? yes or no, no caps.");
+    if (funny == ("yes")){var funny = 1}
+    else if (funny == ("no")){var funny = 0}
+    else (funny == ("")){var funny = prompt ("Would it be funny? yes or no, no caps.");}
+var result = Number(ppl)+Number(funny)
+
+    (result==2)? console.log("yes"):console.log("no");
