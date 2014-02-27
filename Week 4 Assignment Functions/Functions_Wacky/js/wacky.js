33a439bd43384ad1323d54ac43d15647edc3fe1f
Graevus <graevus427@gmail.com>
2014-02-26 19:14:56 -0600

    Commit Five. Working out some more bugs.

--- Week 4 Assignment Functions/Functions_Wacky/js/wacky.js
+++ Week 4 Assignment Functions/Functions_Wacky/js/wacky.js
@@ -3,14 +3,14 @@
 //Functions - Wacky
 
 var ppl  = prompt("Are there other people around? Answer yes or no, no caps.");//Prompting user for the size of gas tank.
-    if (ppl =="yes"){var ppl = 1}
-    else if (ppl == "no"){var ppl = 0}
-    else if (ppl == ""){var ppl = prompt ("Are there other people around? Answer yes or no, no caps.");}
+    if (ppl == ""){var ppl = prompt ("Are there other people around? Answer yes or no, no caps.");}
+    else if (ppl == "no"){var ppl = 0;}
+    else if (ppl =="yes"){var ppl = 1;}
 
 var funny = prompt("Would it be funny? yes or no, no caps.");
-    if (funny == "yes"){var funny = 1}
-    else if (funny == "no"){var funny = 0}
-    else if (funny == ""){var funny = prompt ("Would it be funny? yes or no, no caps.");}
+    if (funny == ""){var funny = prompt ("Would it be funny? yes or no, no caps.");}
+    else if (funny == "no"){var funny = 0;}
+    else if (funny == "yes"){var funny = 1;}
 
 var calc=function(a,b){
     var add =Number(a)+Number(b);
