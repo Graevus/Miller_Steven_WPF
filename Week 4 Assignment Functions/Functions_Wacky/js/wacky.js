d1741c8bd1de693d952f240c10637ea63bab2aa1
Graevus <graevus427@gmail.com>
2014-02-26 19:35:46 -0600

    Commit Nine. More comments.

--- Week 4 Assignment Functions/Functions_Wacky/js/wacky.js
+++ Week 4 Assignment Functions/Functions_Wacky/js/wacky.js
@@ -13,8 +13,8 @@ var funny = prompt("Would it be funny?/n" +
     "yes or no, no caps.");//Prompting user for answer to question.
     if (funny == ""){var funny = prompt ("Would it be funny? /n" +
         "yes or no, no caps.");}//Repeating the prompt in case it was left blank the first time.
-    else if (funny == "no"){var funny = 0;}
-    else if (funny == "yes"){var funny = 1;}
+    else if (funny == "no"){var funny = 0;}//Declaring the value of result for later purpose.
+    else if (funny == "yes"){var funny = 1;}//Declaring the value of result for later purpose.
 
 var calc=function(a,b){
     var add =Number(a)+Number(b);
