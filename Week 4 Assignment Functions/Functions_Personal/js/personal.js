526040b06fb28586d25a902b5dd09147840036b2
Graevus <graevus427@gmail.com>
2014-02-26 18:08:15 -0600

    Commit one. Have most of the code laid out. Debugging and cleaning it up next.

---
+++ Week 4 Assignment Functions/Functions_Personal/js/personal.js
@@ -0,0 +1,18 @@
+//Steven Miller
+//02/26/14
+//Functions - Personal
+
+var tank  = prompt("What is the size of your gas tank?");//Prompting user for the size of gas tank.
+    if (tank == ("")){var tank = prompt ("What is the size of your gas tank?");}//Repeating the prompt in case the prompt was left blank the first time.
+
+var mile  = prompt("How many miles do you average per tank of fuel?");//Prompting user for average number of miles per tank of fuel.
+    if (mile == ("")){var mile = prompt ("How many miles do you average per tank of fuel?");}//Repeating the prompt in case the prompt was left blank the first time.
+
+var mpg = function(a,b){
+    var calc = a/b;
+    return calc;
+
+}
+var result = mpg(mile,tank);
+
+console.log("You are getting on average "+result+" mpg.")
